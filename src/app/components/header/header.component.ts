da8f6292601d76bbe4d2a76e64f98a2cbf021bc6
nicoKoehler <nk.logins@pm.me>
2022-02-26 11:09:34 +0100

    task app finished

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,7 @@ export class HeaderComponent implements OnInit {
   showAddTask!: boolean;
   subscription!: Subscription;
 
-  constructor(private uiService:UiService) { 
+  constructor(private uiService:UiService, private router:Router) { 
     this.subscription = this.uiService.sv_onToggle().subscribe(value => this.showAddTask = value)
 
   }
@@ -24,4 +25,9 @@ export class HeaderComponent implements OnInit {
     this.uiService.sv_toggleAddTask();
 
   }
+
+  hasRoute(route: string){
+    return this.router.url === route;
+  }
+
 }
